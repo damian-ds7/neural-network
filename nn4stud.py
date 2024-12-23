dc0e13b72a17e4496c1f2baf55b438ee357d07e4
Kamil Marszalek <kamilmar553@gmail.com>
2024-12-23 20:43:45 +0100

    Enabled to change activation func

--- nn4stud.py
+++ nn4stud.py
@@ -16,6 +16,7 @@ import numpy as np
 #ToDo tu prosze podac pierwsze cyfry numerow indeksow
 p = [1, 8]
 
+
 L_BOUND = -5
 U_BOUND = 5
 
@@ -36,7 +37,9 @@ def sigmoid(x):
 def d_sigmoid(x):
     s = 1/(1+np.exp(-x))
     return s * (1-s)
-     
+
+FUNCTION = sigmoid
+D_FUNCTION = d_sigmoid
 #f. straty
 def nloss(y_out, y):
     return (y_out - y) ** 2
@@ -51,8 +54,8 @@ class DlNet:
         self.y = y
         self.y_out = 0
         
-        self.HIDDEN_L_SIZE = 14
-        self.LR = 0.05
+        self.HIDDEN_L_SIZE = 50
+        self.LR = 0.005
 
         self.W1 = np.random.randn(1, self.HIDDEN_L_SIZE) #weights between input and hidden layer
         self.b1 = np.random.randn(1, self.HIDDEN_L_SIZE) #bias between input and hidden layer
@@ -63,7 +66,7 @@ class DlNet:
     def forward(self, x):  
         x_res = x.reshape(-1,1)
         self.z1 = np.dot(x_res, self.W1) + self.b1
-        self.a1 = sigmoid(self.z1)
+        self.a1 = FUNCTION(self.z1)
         self.z2 = np.dot(self.a1, self.W2) + self.b2
         self.y_out = self.z2
         return self.y_out       
@@ -84,7 +87,7 @@ class DlNet:
         dLoss_db2 = np.sum(dLoss_dz2, axis=0, keepdims=True) / n
 
         dLoss_da1 = np.dot(dLoss_dz2, self.W2.T)
-        dLoss_dz1 = dLoss_da1 * d_sigmoid(self.z1)
+        dLoss_dz1 = dLoss_da1 * D_FUNCTION(self.z1)
 
         dLoss_dW1 = np.dot(x_res.T, dLoss_dz1) / n
         dLoss_db1 = np.sum(dLoss_dz1, axis=0, keepdims=True) / n
@@ -102,7 +105,7 @@ class DlNet:
 
         
 nn = DlNet(x,y)
-nn.train(x, y, 100000)
+nn.train(x, y, 1000000)
 
 
 yh = nn.predict(x).flatten()
